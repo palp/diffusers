fec13307ad195d0204af6a0f3e56458ec0fa682e
huoju <virushuo@gmail.com>
2023-03-12 08:22:20 +0000

    cross attention upcast fixed

--- src/diffusers/models/cross_attention.py
+++ src/diffusers/models/cross_attention.py
@@ -200,34 +200,35 @@ class CrossAttention(nn.Module):
 
     def get_attention_scores(self, query, key, attention_mask=None):
         dtype = query.dtype
-        if self.upcast_attention:
-            query = query.float()
-            key = key.float()
+        with torch.autocast("cuda", enabled=False):
+            if self.upcast_attention:
+                query = query.float()
+                key = key.float()
 
-        if attention_mask is None:
-            baddbmm_input = torch.empty(
-                query.shape[0], query.shape[1], key.shape[1], dtype=query.dtype, device=query.device
+            if attention_mask is None:
+                baddbmm_input = torch.empty(
+                    query.shape[0], query.shape[1], key.shape[1], dtype=query.dtype, device=query.device
+                )
+                beta = 0
+            else:
+                baddbmm_input = attention_mask
+                beta = 1
+
+            attention_scores = torch.baddbmm(
+                baddbmm_input,
+                query,
+                key.transpose(-1, -2),
+                beta=beta,
+                alpha=self.scale,
             )
-            beta = 0
-        else:
-            baddbmm_input = attention_mask
-            beta = 1
 
-        attention_scores = torch.baddbmm(
-            baddbmm_input,
-            query,
-            key.transpose(-1, -2),
-            beta=beta,
-            alpha=self.scale,
-        )
+            if self.upcast_softmax:
+                attention_scores = attention_scores.float()
 
-        if self.upcast_softmax:
-            attention_scores = attention_scores.float()
+            attention_probs = attention_scores.softmax(dim=-1)
+            attention_probs = attention_probs.to(dtype)
 
-        attention_probs = attention_scores.softmax(dim=-1)
-        attention_probs = attention_probs.to(dtype)
-
-        return attention_probs
+            return attention_probs
 
     def prepare_attention_mask(self, attention_mask, target_length, batch_size=None):
         if batch_size is None:
